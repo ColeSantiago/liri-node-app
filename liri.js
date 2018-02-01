a3aca0229d5d56d762d087efd224d42686d57fec
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-02-01 17:16:51 -0500

    twitter api is working

--- liri.js
+++ liri.js
@@ -1,6 +1,37 @@
 require("dotenv").config();
 
-const keys = require('keys');
+const keys = require("./keys");
 
-let spotify = new Spotify(keys.spotify);
-let client = new Twitter(keys.twitter);
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
+
+let params = {
+  count: 20,
+  exclude_replies: 'true'
+}
+
+client.get("statuses/home_timeline", params, function(error, tweets, response) {
+
+	// console.log(tweets);
+
+	if (!error) {
+
+		 for (let i = 0; i < tweets.length; i++) {
+	      
+	      let date = tweets[i].created_at
+	      let text = tweets[i].text
+
+	      console.log(date);
+	      console.log(text);
+	     
+	     
+     	}
+    }
+
+});
+
+		
+
