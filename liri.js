6829e81277b0c63202c63f36d6b80fa818a95d1a
Cole Santiago <colesantiago@Coles-MacBook-Pro.local>
2018-02-02 17:14:43 -0500

    first part of omdb api is working. still need to set default movie

--- liri.js
+++ liri.js
@@ -4,16 +4,14 @@ const keys = require("./keys");
 
 const Twitter = require('twitter');
 const Spotify = require('node-spotify-api');
+const request = require("request");
 
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
 
-let params = {
-  count: 20,
-  exclude_replies: 'true'
-}
-
 let arg = process.argv[2];
+
+//directions if user input is blank
 
 	if (!arg) {
 
@@ -23,6 +21,11 @@ let arg = process.argv[2];
 	}
 
 // twitter api
+
+let params = {
+  count: 20,
+  exclude_replies: 'true'
+}
 
 	if (arg === 'my-tweets') {
 
@@ -71,7 +74,7 @@ let arg = process.argv[2];
 				spotify.search({ type: 'track', query: 'swimming in the moonlight' }, function(err, data) {
 
 					if (!err) {
-						
+
 						console.log('Artist: ' + data.tracks.items[0].artists[0].name);
 						console.log('Track: ' + data.tracks.items[0].name);
 						console.log('Preview link: ' + data.tracks.items[0].preview_url);
@@ -82,7 +85,48 @@ let arg = process.argv[2];
 
 	}
 
-			
+// omdb api
+
+	if (arg === 'movie-this') { 
+
+		let movie = process.argv;
+		let movieSearch = "";
+
+			for (let i = 3; i < movie.length; i++) {
+
+			  if (i > 3 && i < movie.length) {
+
+			    movieSearch = movieSearch + "+" + movie[i];
+
+			  } else {
+
+			    movieSearch += movie[i];
+
+			  }
+			}
+
+		const queryUrl = "http://www.omdbapi.com/?t=" + movieSearch + "&y=&plot=short&apikey=trilogy";
+
+		request(queryUrl, function(error, response, body) {
+
+	  		if (!error && response.statusCode === 200) {
+
+	    		console.log("Title: " + JSON.parse(body).Title);
+	    		console.log("Release Year: " + JSON.parse(body).Year);
+	    		console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+
+	    		const ratingObject = JSON.parse(body).Ratings;
+	    		console.log("Rotten Tomatoes Rating: " + (ratingObject[1].Value));
+
+	    		console.log("Country: " + JSON.parse(body).Country);
+	    		console.log("Language: " + JSON.parse(body).Language);
+	    		console.log("Plot: " + JSON.parse(body).Plot);
+	    		console.log("Actors: " + JSON.parse(body).Actors);
+
+	  		}
+		})
+	}
+
 	
 
 
